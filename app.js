57f9961f24ee7c95ad1dc2375620717d83516290
akanshaSH123 <akku.at20@gmail.com>
2023-12-20 15:30:11 +0530

    calculator codes

---
+++ app.js
@@ -0,0 +1,35 @@
+var allTd = document.querySelectorAll('#container td');
+
+var screen = document.querySelector('#container input')
+
+
+// console.log(allTd);
+
+for (let i = 0; i < allTd.length; i++) {
+    allTd[i].addEventListener('click', (e) => {
+        var text = e.target.innerText;
+        screen.value += text;
+
+        if (text === '=') {
+            try {
+                screen.value = eval(screen.value);
+            } catch (e) {
+                prompt('invalid operation')
+                screen.value = '';
+            }
+        } else if (text === 'X') {
+            screen.value += '*';
+        }
+        else if (text === 'C') {
+            screen.value = '';
+        }
+        else if (text === 'X2') {
+            screen.value += '**'
+        }
+
+        else {
+            screen.value += text;
+        }
+    })
+}
+
